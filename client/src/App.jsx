32d88200600bae7fa4d044c548548fe3d65b7dea
Ramal <vrasdasd707@gmail.com>
2024-01-16 11:10:22 +0400

    back-end completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
-function App() {
-  return (<>
-    <h1>Salam</h1>
-    </>);
+import React from 'react'
+import Home from './pages/home'
+const App = () => {
+  return (
+    <div>
+    <Home>
+    </Home>
+    </div>
+  )
 }
 
-export default App;
+export default App
